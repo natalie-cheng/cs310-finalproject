e3528f0e999033e41bbac1e332bae2861281c2d4
Natalie Cheng <nataliecheng2026@u.northwestern.edu>
2024-05-21 17:08:50 -0500

    trying different clients

---
+++ client/main.py
@@ -0,0 +1,143 @@
+import requests
+import jsons
+
+import uuid
+import pathlib
+import logging
+import sys
+import os
+import base64
+import time
+
+from configparser import ConfigParser
+
+############################################################
+#
+# prompt
+#
+def prompt():
+  """
+  Prompts the user and returns the command number
+
+  Parameters
+  ----------
+  None
+
+  Returns
+  -------
+  Command number entered by user (0, 1, 2, ...)
+  """
+  print()
+  print(">> Enter a command:")
+  print("   0 => end")
+  print("   1 => users")
+  print("   2 => jobs")
+  print("   3 => reset database")
+  print("   4 => upload pdf")
+  print("   5 => download results")
+  print("   6 => upload and poll")
+
+  cmd = input()
+
+  if cmd == "":
+    cmd = -1
+  elif not cmd.isnumeric():
+    cmd = -1
+  else:
+    cmd = int(cmd)
+
+  return cmd
+
+############################################################
+# main
+#
+try:
+  print('** Welcome to SongSpot **')
+  print()
+
+  # eliminate traceback so we just get error message:
+  sys.tracebacklimit = 0
+
+  #
+  # what config file should we use for this session?
+  #
+  config_file = 'spotify-client-config.ini'
+
+  print("Config file to use for this session?")
+  print("Press ENTER to use default, or")
+  print("enter config file name>")
+  s = input()
+
+  if s == "":  # use default
+    pass  # already set
+  else:
+    config_file = s
+
+  #
+  # does config file exist?
+  #
+  if not pathlib.Path(config_file).is_file():
+    print("**ERROR: config file '", config_file, "' does not exist, exiting")
+    sys.exit(0)
+
+  #
+  # setup base URL to web service:
+  #
+  configur = ConfigParser()
+  configur.read(config_file)
+  baseurl = configur.get('client', 'webservice')
+
+  #
+  # make sure baseurl does not end with /, if so remove:
+  #
+  if len(baseurl) < 16:
+    print("**ERROR: baseurl '", baseurl, "' is not nearly long enough...")
+    sys.exit(0)
+
+  if baseurl == "https://YOUR_GATEWAY_API.amazonaws.com":
+    print("**ERROR: update config file with your gateway endpoint")
+    sys.exit(0)
+
+  if baseurl.startswith("http:"):
+    print("**ERROR: your URL starts with 'http', it should start with 'https'")
+    sys.exit(0)
+
+  lastchar = baseurl[len(baseurl) - 1]
+  if lastchar == "/":
+    baseurl = baseurl[:-1]
+
+  #
+  # main processing loop:
+  #
+  cmd = prompt()
+
+  while cmd != 0:
+    #
+    if cmd == 1:
+      print("hello")
+    # elif cmd == 2:
+    #   jobs(baseurl)
+    # elif cmd == 3:
+    #   reset(baseurl)
+    # elif cmd == 4:
+    #   upload(baseurl)
+    # elif cmd == 5:
+    #   download(baseurl)
+    # elif cmd == 6:
+    #   upload_and_poll(baseurl)
+    else:
+      print("** Unknown command, try again...")
+    #
+    cmd = prompt()
+
+  #
+  # done
+  #
+  print()
+  print('** done **')
+  sys.exit(0)
+
+except Exception as e:
+  logging.error("**ERROR: main() failed:")
+  logging.error(e)
+  sys.exit(0)
